8c083552ee88bbfcaa6ab97c2ca8970dc16d56af
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-11-29 17:19:19 +0800

    Restructured home and hero components

---
+++ client/src/components/Hero.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Typography, Button, Box } from "@mui/material";
+// import backgroundImage from "../images/starrybackground3.jpg";
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        // background: `url(${backgroundImage}) center/cover no-repeat fixed`,
+        backgroundColor: '#141416' ,
+        height: "100vh",
+        color: '#f5f5f5',
+      }}
+    >
+      <Container sx={{ mx: "2em", pt: "10em" }}>
+        <Box sx={{ textAlign: "left" }}>
+          <Typography variant="h1" >
+            Unlock Your Beat
+          </Typography>
+          <Typography variant="h2" sx={{ my: "1em"}}>
+            Your Personalized Music Analytics Hub
+          </Typography>
+          <Button
+            variant="contained"
+            color="success"
+            size="large"
+            sx={{ py: "1em" }}
+          >
+            Get Started
+          </Button>
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
